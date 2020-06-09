c8b8690b4931e18725f43bf47e2c8d4ca7a99061
Alison Veras <alisson_veras@hotmail.com>
2020-06-09 00:48:48 -0300

    02_01-Configurando estrutura

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, resp) => {
+  return resp.json({ message: 'Hello World!'});
+})
+
+app.listen((3333), () => {
+  console.log('Server started on port 3333');
+});
+
