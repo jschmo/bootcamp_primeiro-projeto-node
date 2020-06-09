80b66f4024f1384e861f43f2a54290f8a8a4b768
Alison Veras <alisson_veras@hotmail.com>
2020-06-09 01:39:40 -0300

    02_03-ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, resp) => {
-  return resp.json({ message: 'Hello World!'});
-})
+app.get('/', (req, resp) => resp.json({ message: 'Hello World!' }));
 
 app.listen((3333), () => {
   console.log('Server started on port 3333');
