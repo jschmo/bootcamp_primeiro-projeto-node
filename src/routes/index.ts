ff88ce4f5a8d919967161772781f068b8000a293
Alison Veras <alisson_veras@hotmail.com>
2020-06-10 01:50:52 -0300

    02_06-Debugando Node.JS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,11 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (req, resp) => resp.json({ message: 'Hello World!' }));
+routes.post('/users', (req, res) => {
+  const { name, email } = req.body;
+
+  const user = { name, email };
+  return res.json(user);
+});
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import routes from './routes/routes';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
