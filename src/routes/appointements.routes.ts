25631cab904840052c3cdda12b62410a467ccd85
Alison Veras <alisson_veras@hotmail.com>
2020-06-11 01:52:10 -0300

    02_08-Validando a data

--- src/routes/appointements.routes.ts
+++ src/routes/appointements.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
-  const appointment = {
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInTheSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInTheSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+  const appointment: Appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
   appointments.push(appointment);
   return res.json(appointment);
