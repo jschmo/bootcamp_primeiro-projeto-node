b52c635d2b8b7c286438ab6e70471d931f27ae95
Alison Veras <alisson_veras@hotmail.com>
2020-06-16 02:33:41 -0300

    02_19-Relacionamento nos models

--- src/database/migrations/1592193729827-CreateAppointments.ts
+++ src/database/migrations/1592193729827-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1592193729827
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
